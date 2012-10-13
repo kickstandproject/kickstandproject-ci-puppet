7cf63494660ce7f67e0cff489527d7617d32c196
Paul Belanger <paul.belanger@polybeacon.com>
2012-10-13 15:28:29 -0400

    Also add node for jenkins-01-prod
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,7 @@
+node 'jenkins-01-prod.kickstand-project.org' {
+  class { 'kickstandproject::node::jenkins::init': }
+}
+
 node 'rebuildd-01-prod.kickstand-project.org' {
   class { 'kickstandproject::node::rebuildd::init': }
 }
