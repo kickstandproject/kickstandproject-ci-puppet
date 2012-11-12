87517c2071a34849b276939169c7ae086f502872
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-12 11:29:58 -0500

    Add puppet-lint job for our modules
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/jenkins/config.pp
+++ modules/kickstandproject/manifests/node/jenkins/config.pp
@@ -5,13 +5,11 @@
 #
 class kickstandproject::node::jenkins::config {
   jenkins_job_builder::function::template { 'defaults.yaml':
-    content =>
-      template('kickstandproject/jenkins/etc/jenkins_jobs/configs/defaults.yaml.erb'),
+    content => template('kickstandproject/jenkins/etc/jenkins_jobs/configs/defaults.yaml.erb'),
   }
 
-  jenkins_job_builder::function::template { 'zuul.yaml':
-    content =>
-      template('kickstandproject/jenkins/etc/jenkins_jobs/configs/zuul.yaml.erb'),
+  jenkins_job_builder::function::template { 'kickstandproject-ci-puppet.yaml':
+    content => template('kickstandproject/jenkins/etc/jenkins_jobs/configs/kickstandproject-ci-puppet.yaml.erb'),
   }
 }
 
