31407e1a7d36cdc8eb9733adedf1d0fa11cd8be2
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-12 11:15:07 -0500

    Add puppet::server class
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/puppet/init.pp
+++ modules/kickstandproject/manifests/node/puppet/init.pp
@@ -26,6 +26,12 @@ class kickstandproject::node::puppet::init {
   class { 'network::client':
     interfaces  => $network_interfaces,
   }
+
+  class { 'puppet::server':
+    options => {
+      'modulepath' => '/opt/puppet-modules/$environment/modules:/etc/puppet/modules',
+    }
+  }
 }
 
 # vim:sw=2:ts=2:expandtab:textwidth=79
