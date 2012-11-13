b2ab8caedf54d19c30da804e3d9c29a23c2f8303
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-12 19:06:07 -0500

    Add ppa:pabelanger/zuul to precise host
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/precise/bootstrap.pp
+++ modules/kickstandproject/manifests/node/precise/bootstrap.pp
@@ -6,6 +6,11 @@
 class kickstandproject::node::precise::bootstrap(
   $stage = 'bootstrap',
 ) {
+  apt::function::repository { 'launchpad-pabelanger-zuul':
+    components  => 'main',
+    key         => '2C9F0613',
+    url         => 'ppa.launchpad.net/pabelanger/zuul/ubuntu',
+  }
 }
 
 # vim:sw=2:ts=2:expandtab:textwidth=79
