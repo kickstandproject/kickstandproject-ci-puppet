fd103dd4d39fca0799480e74d5e3d4398a6214d2
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-21 11:00:40 -0500

    Add mysql::server
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/gerrit/init.pp
+++ modules/kickstandproject/manifests/node/gerrit/init.pp
@@ -24,6 +24,8 @@ class kickstandproject::node::gerrit::init {
   class { 'network::client':
     interfaces  => $network_interfaces,
   }
+
+  class { 'mysql::server': }
 }
 
 # vim:sw=2:ts=2:expandtab:textwidth=79
