75449658dab1ea3fb3dccc87b780ad6dcd490117
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-23 15:27:59 -0500

    Add zuul system user
    Change-Id: Ic79d01a0a82ecc91e2b7d51f20c532ed0037df5d
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/jenkins/config.pp
+++ modules/kickstandproject/manifests/node/jenkins/config.pp
@@ -11,6 +11,13 @@ class kickstandproject::node::jenkins::config {
   jenkins_job_builder::function::template { 'kickstandproject-ci-puppet.yaml':
     content => template('kickstandproject/jenkins/etc/jenkins_jobs/configs/kickstandproject-ci-puppet.yaml.erb'),
   }
+
+  # TODO: Move into kickstandproject-zuul
+  user { 'zuul':
+    ensure => present,
+    home   => '/var/lib/zuul',
+    system => true,
+  }
 }
 
 # vim:sw=2:ts=2:expandtab:textwidth=79
