5ef363a52543af2e24bba5d1cf8d1dba69d9f928
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-23 16:25:37 -0500

    Remove zuul user!
    Change-Id: I377bf4094f573f6058d99e5ac9003ada79977903
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/jenkins/config.pp
+++ modules/kickstandproject/manifests/node/jenkins/config.pp
@@ -11,15 +11,6 @@ class kickstandproject::node::jenkins::config {
   jenkins_job_builder::function::template { 'kickstandproject-ci-puppet.yaml':
     content => template('kickstandproject/jenkins/etc/jenkins_jobs/configs/kickstandproject-ci-puppet.yaml.erb'),
   }
-
-  # TODO: Move into kickstandproject-zuul
-  user { 'zuul':
-    ensure     => present,
-    home       => '/var/lib/zuul',
-    managehome => true,
-    shell      => '/bin/false',
-    system     => true,
-  }
 }
 
 # vim:sw=2:ts=2:expandtab:textwidth=79
