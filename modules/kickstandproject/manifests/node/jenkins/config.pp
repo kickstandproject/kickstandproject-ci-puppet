04f022e8e72e0c8f776bbd833809acfd067e8eac
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-25 16:38:54 -0500

    Fix templates typo
    I really need a gate check for puppet --noop
    
    Change-Id: I86d252e3f3b0b9774443c1b31a1b59c8b9ac6527
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/jenkins/config.pp
+++ modules/kickstandproject/manifests/node/jenkins/config.pp
@@ -8,16 +8,16 @@ class kickstandproject::node::jenkins::config {
     content => template('kickstandproject/jenkins/etc/jenkins_jobs/configs/defaults.yaml.erb'),
   }
 
-  jenkins_job_builder::function::template { 'puppet.yaml':
-    content => template('kickstandproject/jenkins/etc/jenkins_jobs/configs/puppet.yaml.erb'),
-  }
-
   jenkins_job_builder::function::template { 'macros.yaml':
     content => template('kickstandproject/jenkins/etc/jenkins_jobs/configs/macros.yaml.erb'),
   }
 
   jenkins_job_builder::function::template { 'projects.yaml':
     content => template('kickstandproject/jenkins/etc/jenkins_jobs/configs/projects.yaml.erb'),
+  }
+
+  jenkins_job_builder::function::template { 'puppet-jobs.yaml':
+    content => template('kickstandproject/jenkins/etc/jenkins_jobs/configs/puppet-jobs.yaml.erb'),
   }
 
   jenkins_job_builder::function::template { 'python-jobs.yaml':
