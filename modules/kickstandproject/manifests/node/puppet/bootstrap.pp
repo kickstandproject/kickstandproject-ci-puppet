dcd1b0703669beca2e5f18f06684dbf58e120ed2
Paul Belanger <paul.belanger@polybeacon.com>
2013-02-20 18:40:16 -0500

    Add puppetlabs apt repo
    Change-Id: I31314764c8fea9c47378cea23ffa6dac40d8c34a
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/puppet/bootstrap.pp
+++ modules/kickstandproject/manifests/node/puppet/bootstrap.pp
@@ -1,11 +1,16 @@
 #
-# Copyright (C) 2012, PolyBeacon, Inc.
+# Copyright (C) 2012-2013, PolyBeacon, Inc.
 #
 # Paul Belanger <paul.belanger@polybeacon.com>
 #
 class kickstandproject::node::puppet::bootstrap(
   $stage = 'bootstrap',
 ) {
+  apt::function::repository { 'puppetlabs':
+    components => 'main',
+    key        => '4BD6EC30',
+    url        => 'apt.puppetlabs.com',
+  }
 }
 
-# vim:sw=2:ts=2:expandtab:textwidth=79
+# vim:sw=2:ts=2:expandtab
