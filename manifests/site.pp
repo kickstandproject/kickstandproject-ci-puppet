f862ef6b577442426460f5f3d70e9e6c4128a65f
Paul Belanger <paul.belanger@polybeacon.com>
2013-02-20 23:44:24 -0500

    Remove rebuildd node
    Change-Id: Ie1f90bf7d2873da004678a6aafa4c9b8701585f7
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,8 +14,4 @@ node 'puppet-01-prod.kickstand-project.org' {
   class { 'kickstandproject::node::puppet::init': }
 }
 
-node 'rebuildd-01-prod.kickstand-project.org' {
-  class { 'kickstandproject::node::rebuildd::init': }
-}
-
-# vim:sw=2:ts=2:expandtab:textwidth=79
+# vim:sw=2:ts=2:expandtab
