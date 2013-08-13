58296d43e69d6a8ea24ca4727fa872ab0d97a166
Paul Belanger <paul.belanger@polybeacon.com>
2013-08-13 16:16:00 -0400

    Add vhost for tarballs.ksp.org
    Change-Id: Id519636ef4bfbbea2adb14f69f59f0ede86a7d97
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/static/config.pp
+++ modules/kickstandproject/manifests/node/static/config.pp
@@ -4,6 +4,10 @@
 # Paul Belanger <paul.belanger@polybeacon.com>
 #
 class kickstandproject::node::static::config {
+  require apache::server
+
+  apache::function::virtualhost { 'tarballs.kickstand-project.org': }
+
 }
 
 # vim:sw=2:ts=2:expandtab
