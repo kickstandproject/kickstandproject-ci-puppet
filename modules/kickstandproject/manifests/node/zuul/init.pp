3c7c3fb8938d4c7a9be96c15576b131054aedf04
Paul Belanger <paul.belanger@polybeacon.com>
2013-11-30 19:15:26 +0000

    Create zuul.kickstand-project.org
    It's time for zuul to be given its own hostname.
    
    Change-Id: I1cb676f43be4f009d192c83d50d1e9986d38e15b
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/zuul/init.pp
+++ modules/kickstandproject/manifests/node/zuul/init.pp
@@ -10,7 +10,34 @@ class kickstandproject::node::zuul::init {
     stage => 'bootstrap',
   }
 
-  class { 'kickstandproject::node::zuul::config': }
+  user { 'zuul':
+    ensure     => present,
+    home       => '/var/lib/zuul',
+    managehome => true,
+    shell      => '/bin/false',
+    system     => true,
+  }
+
+  file { '/etc/zuul':
+    ensure  => directory,
+    group   => 'zuul',
+    mode    => '0750',
+    owner   => 'zuul',
+    recurse => true,
+    source  => 'puppet:///modules/kickstandproject/zuul/etc/zuul',
+    require => User['zuul'],
+  }
+
+  file { '/etc/zuul/layout.yaml':
+    ensure  => file,
+    content => template('kickstandproject/zuul/etc/zuul/layout.yaml.erb'),
+    require => File['/etc/zuul'],
+  }
+
+  file { '/etc/init/zuul.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/kickstandproject/zuul/etc/init/zuul.conf',
+  }
 }
 
 # vim:sw=2:ts=2:expandtab
