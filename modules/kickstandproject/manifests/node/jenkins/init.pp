5457168cdfd4d8531d65ba2f2ca558e037fdf260
Paul Belanger <paul.belanger@polybeacon.com>
2013-12-02 03:43:21 +0000

    Rework jenkins node
    Change-Id: I7aca30c2973c88474080f8d807038b85e97768f5
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/jenkins/init.pp
+++ modules/kickstandproject/manifests/node/jenkins/init.pp
@@ -4,49 +4,11 @@
 # Paul Belanger <paul.belanger@polybeacon.com>
 #
 class kickstandproject::node::jenkins::init {
-  $network_auto = [
-    'eth0',
-    'eth1',
-  ]
-
-  $network_interfaces = {
-    'eth0' => {
-      'method'          => 'static',
-      'address'         => '209.87.247.149',
-      'netmask'         => '255.255.255.240',
-      'gateway'         => '209.87.247.145',
-      'dns-nameservers' => '8.8.8.8',
-      'dns-search'      => 'kickstand-project.org',
-    },
-    'eth1' => {
-      'method' => 'dhcp',
-    },
-  }
-
   class { 'kickstandproject::init': }
 
   class { 'kickstandproject::node::jenkins::bootstrap':
     stage => 'bootstrap',
   }
-
-  class { 'network::client':
-    auto       => $network_auto,
-    interfaces => $network_interfaces,
-  }
-
-  class { 'jenkins::server':
-    hostname  => 'jenkins.kickstand-project.org',
-  }
-
-  # TODO: Add these settings to hiera
-  class { 'jenkins_job_builder::client':
-    settings => {
-      'password' => '09933c495f8476a6f021ce7865ad96a8',
-      'url'      => 'http://jenkins.kickstand-project.org',
-    }
-  }
-
-  class { 'kickstandproject::node::jenkins::config': }
 }
 
 # vim:sw=2:ts=2:expandtab
