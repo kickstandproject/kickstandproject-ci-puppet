46f8bb08acd91abf6343f58d8c3e9a40146033d5
Paul Belanger <paul.belanger@polybeacon.com>
2013-12-02 03:43:21 +0000

    Rework jenkins node
    Change-Id: I7aca30c2973c88474080f8d807038b85e97768f5
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/jenkins/init.pp
+++ modules/kickstandproject/manifests/node/jenkins/init.pp
@@ -4,49 +4,60 @@
 # Paul Belanger <paul.belanger@polybeacon.com>
 #
 class kickstandproject::node::jenkins::init {
-  $network_auto = [
-    'eth0',
-    'eth1',
-  ]
-
-  $network_interfaces = {
-    'eth0' => {
-      'method'          => 'static',
-      'address'         => '209.87.247.149',
-      'netmask'         => '255.255.255.240',
-      'gateway'         => '209.87.247.145',
-      'dns-nameservers' => '8.8.8.8',
-      'dns-search'      => 'kickstand-project.org',
-    },
-    'eth1' => {
-      'method' => 'dhcp',
-    },
-  }
-
   class { 'kickstandproject::init': }
 
   class { 'kickstandproject::node::jenkins::bootstrap':
     stage => 'bootstrap',
   }
+  class { 'apache':
+    default_vhost => false,
+  }
+  class { 'apache::mod::proxy':
+    allow_from => 'all',
+  }
+  class { 'apache::mod::proxy_http': }
+  class { 'jenkins::server': }
 
-  class { 'network::client':
-    auto       => $network_auto,
-    interfaces => $network_interfaces,
+  $proxy_pass = [
+    { 'path' => '/', 'url' => 'http://localhost:8080/' },
+  ]
+
+  apache::vhost { 'jenkins.kickstand-project.org':
+    docroot    => '/var/www/jenkins.kickstand-project.org',
+    port       => '80',
+    proxy_pass => $proxy_pass,
   }
 
-  class { 'jenkins::server':
-    hostname  => 'jenkins.kickstand-project.org',
+  firewall { '80 accept - apache':
+    action => 'accept',
+    port   => '80',
+    proto  => 'tcp',
   }
 
-  # TODO: Add these settings to hiera
+  package { 'python-pip':
+    ensure => present,
+  }
+
+  $jjb_package = {
+    ensure   => '0.6.0',
+    provider => 'pip',
+  }
+
   class { 'jenkins_job_builder::client':
+    package  => $jjb_package,
+    require  => Package['python-pip'],
     settings => {
       'password' => '09933c495f8476a6f021ce7865ad96a8',
-      'url'      => 'http://jenkins.kickstand-project.org',
     }
   }
 
-  class { 'kickstandproject::node::jenkins::config': }
+  file { '/etc/jenkins_jobs/configs':
+    ensure  => directory,
+    notify  => Exec['jenkins-jobs-update'],
+    recurse => true,
+    require => Class['jenkins_job_builder::client'],
+    source  => 'puppet:///modules/kickstandproject/jenkins/etc/jenkins_jobs/configs',
+  }
 }
 
 # vim:sw=2:ts=2:expandtab
