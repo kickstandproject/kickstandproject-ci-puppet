9130095cce12fc57c3545942509bddb3caa86ebb
Paul Belanger <paul.belanger@polybeacon.com>
2014-03-25 18:38:21 +0000

    Upgrade Jenkins to use upstream packages
    We need to upgrade Jenkins to leverage using nodepool. Now we use upstreams
    debian package rather then the one we hacked together on launchpad.
    
    Change-Id: Id139b3c8c591d2160beda9424c3af82542551f2f
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/jenkins/bootstrap.pp
+++ modules/kickstandproject/manifests/node/jenkins/bootstrap.pp
@@ -5,9 +5,10 @@
 #
 class kickstandproject::node::jenkins::bootstrap {
   apt::function::repository { 'launchpad-pabelanger-zuul':
-    components  => 'main',
-    key         => '2C9F0613',
-    url         => 'ppa.launchpad.net/pabelanger/zuul/ubuntu',
+    components  => 'binary/',
+    dist        => '',
+    key         => 'D50582E6',
+    url         => 'pkg.jenkins-ci.org/debian',
   }
 }
 
