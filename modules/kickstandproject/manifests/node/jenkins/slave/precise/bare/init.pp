2f546e1ada0d82d2131381e9fd66be46d4e5e893
Paul Belanger <paul.belanger@polybeacon.com>
2014-03-25 17:00:25 +0000

    Enable puppet tests on bare-precise
    Our first task is to get our puppet tests running on bare-precise, then we'll
    be able to start merging in all the new code.
    
    Change-Id: I53725ebfeec60809cbd21bebf18b9c24bb76cf2d
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- modules/kickstandproject/manifests/node/jenkins/slave/precise/bare/init.pp
+++ modules/kickstandproject/manifests/node/jenkins/slave/precise/bare/init.pp
@@ -9,6 +9,34 @@ class kickstandproject::node::jenkins::slave::precise::bare::init {
   class { 'kickstandproject::node::jenkins::slave::precise::bare::bootstrap':
     stage => 'bootstrap',
   }
+
+  $packages = [
+    'rubygems',
+  ]
+
+  package { $packages:
+    ensure => present,
+  }
+
+  $gem_packages = [
+    'puppet-lint',
+    'puppetlabs_spec_helper',
+  ]
+
+  # TODO(pabelanger): Workaround Error installing puppetlabs_spec_helper:
+  #                              rake requires Ruby version >= 1.9.
+  package { 'rake':
+    ensure   => '10.1.1',
+    provider => gem,
+    before   => Package['puppetlabs_spec_helper'],
+    require  => Package['rubygems'],
+  }
+
+  package { $gem_packages:
+    ensure   => latest,
+    provider => gem,
+    require  => Package['rubygems'],
+  }
 }
 
 # vim:sw=2:ts=2:expandtab
