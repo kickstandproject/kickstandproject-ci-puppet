94938f9d4786159afc7f8554e9a9607bfcdd06dd
Paul Belanger <paul.belanger@polybeacon.com>
2014-04-02 15:31:56 -0400

    Pin facter to 1.x
    Change-Id: I6c1a410baa2e83deb204d98ed0454f05664ec692
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- tools/install_puppet.sh
+++ tools/install_puppet.sh
@@ -3,6 +3,10 @@ cat > 00-puppet.pref <<EOF
 Package: puppet puppet-common puppetmaster puppetmaster-common puppetmaster-passenger
 Pin: version 2.7*
 Pin-Priority: 501
+
+Package: facter
+Pin: version 1.*
+Pin-Priority: 501
 EOF
 
 sudo mv 00-puppet.pref /etc/apt/preferences.d/00-puppet.pref
