6612d438b57752461a74137af3ec1c160c3def62
Paul Belanger <paul.belanger@polybeacon.com>
2014-04-06 21:52:18 +0000

    Create 2048 swapfile if none exists
    Change-Id: Icd1d3b46dfb90e0520242ab0cd13aabd5dce5960
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ modules/kickstandproject/manifests/node/base/swap.pp
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2012-2014, PolyBeacon, Inc.
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+class kickstandproject::node::base::swap(
+  $stage = 'bootstrap',
+) {
+  case $::swapsize_mb {
+    '0.00': {
+      exec { 'Create swap file':
+        command => 'dd if=/dev/zero of=/mnt/swap.1 bs=1M count=2048',
+        creates => '/mnt/swap.1',
+        notify  => Exec['Make swap file'],
+      }
+
+      exec { 'Make swap file':
+        command     => 'mkswap /mnt/swap.1 && swapon /mnt/swap.1',
+        notify      => File['/etc/cron.d/swapon'],
+        refreshonly => true,
+      }
+
+      file { '/etc/cron.d/swapon':
+        ensure  => file,
+        group   => 'root',
+        mode    => '0755',
+        owner   => 'root',
+        require => Exec['Create swap file'],
+        source  => 'puppet:///modules/kickstandproject/base/etc/cron.d/swapon',
+      }
+    }
+    default: {}
+  }
+}
+
+# vim:sw=2:ts=2:expandtab
